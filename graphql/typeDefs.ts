27b2998fd2a34c616d6d6a65ac09dd6504622f37
Yanlok Li <yli@bol.com>
2022-04-25 15:39:12 +0200

    2-create-api-route

---
+++ graphql/typeDefs.ts
@@ -0,0 +1,11 @@
+import { gql } from 'apollo-server-micro'
+
+export const typeDefs = gql`
+  type Query {
+    greeting: Greeting
+  }
+
+  type Greeting {
+    message: String
+  }
+`
---
+++ pages/api/graphql.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { ApolloServer } from 'apollo-server-micro'
+
+import { typeDefs } from '@/graphql/typeDefs'
+import { resolvers } from '@/graphql/resolvers'
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+const startServer = apolloServer.start()
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://studio.apollographql.com'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  if (req.method === 'OPTIONS') {
+    res.end()
+    return false
+  }
+
+  await startServer
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+export default handler
