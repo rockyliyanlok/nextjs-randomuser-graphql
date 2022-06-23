faf52c66da5cc01aaee89a83dfb681fdbed164c9
Yanlok Li <yli@bol.com>
2022-06-23 22:34:52 +0200

    10-allow-access-from-all-domains

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const isProd = () => process.env.NODE_ENV === 'production'
 const ContentSecurityPolicy = `
   default-src 'self' apollo-server-landing-page.cdn.apollographql.com;
   script-src 'self' 'unsafe-inline'${!isProd() ? ' \'unsafe-eval\'' : ''} apollo-server-landing-page.cdn.apollographql.com cdn.jsdelivr.net;
-  connect-src 'self' vitals.vercel-insights.com fonts.googleapis.com cdn.jsdelivr.net;
-  style-src 'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com cdn.jsdelivr.net;
+  connect-src 'self' vitals.vercel-insights.com fonts.googleapis.com fonts.gstatic.com cdn.jsdelivr.net;
+  style-src 'self' 'unsafe-inline' fonts.googleapis.com cdn.jsdelivr.net;
   font-src 'self' fonts.gstatic.com;
   img-src 'self' apollo-server-landing-page.cdn.apollographql.com cdn.jsdelivr.net;
 `
